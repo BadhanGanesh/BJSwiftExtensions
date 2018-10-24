942f15b8bd7b4d1b4c5427dc8f309fbdd19882ef
BadhanGanesh <ganesh_randy@yahoo.com>
2018-10-25 00:54:26 +0530

    - Added more flavour to the example project. - Always embed Swift Standard Library is ON now.

--- swiftextensions/ViewController.swift
+++ swiftextensions/ViewController.swift
@@ -9,13 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let cornerRadiusAmount = (self.view.viewWithTag(100)?.bounds.midX)!
+        self.view.viewWithTag(100)?.setCornerRadius(cornerRadiusAmount,
+                                                    borderWidthAmount: 0,
+                                                    borderColor: .clear)
     }
-
+    
     @IBAction func pinButtonTouched(_ sender: UIButton) {
-        self.view.viewWithTag(100)?.pinTo(UIViewPinPosition(rawValue: sender.tag)!)
+        UIView.animate(withDuration: 0.3,
+                       delay: 0,
+                       usingSpringWithDamping: 0.75,
+                       initialSpringVelocity: 0.5,
+                       options: .curveEaseOut,
+                       animations: {
+                        self.view.viewWithTag(100)?.pinTo(UIViewPinPosition(rawValue: sender.tag)!)
+        },
+                       completion: nil)
     }
     
 }
